9f7589efefb72d52009dae447bd5dd188b12f0bc
Ryan Rempel <rgrempel@gmail.com>
2018-09-10 11:36:56 -0500

    Remove some docs that don't apply to Elm 0.18.

--- src/Platform019.elm
+++ src/Platform019.elm
@@ -15,16 +15,6 @@ This is great if you want to use Elm as the &ldquo;brain&rdquo; for something
 else. For example, you could send messages out ports to modify the DOM, but do
 all the complex logic in Elm.
 
-[headless]: https://en.wikipedia.org/wiki/Headless_software
-
-Initializing a headless program from JavaScript looks like this:
-
-```javascript
-var app = Elm.MyThing.init();
-```
-
-[headless]: https://en.wikipedia.org/wiki/Headless_software
-
 -}
 worker :
     { init : flags -> ( model, Cmd msg )
